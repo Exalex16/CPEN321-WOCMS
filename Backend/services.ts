bfacb144284f66a58d88daf50a29067b05fb4c8e
Exalex16 <exalex16@gmail.com>
2025-02-18 20:20:54 -0800

    fix

--- Backend/services.ts
+++ Backend/services.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import AWS from "aws-sdk";
+// import AWS from "aws-sdk";
 // import dotenv from "dotenv";
 
 // dotenv.config();
@@ -8,6 +8,6 @@ import AWS from "aws-sdk";
 export const clinet = new MongoClient(process.env.DB_URI ?? "mongodb://localhost:27017")
 
 // AWS S3 Connection
-export const s3 = new AWS.S3({
-    region: process.env.AWS_REGION
-});
+// export const s3 = new AWS.S3({
+//     region: process.env.AWS_REGION
+// });
