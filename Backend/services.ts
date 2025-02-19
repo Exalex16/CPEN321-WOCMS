2a5d992c5a624a4a6026b3084d574e285c8f5951
Exalex16 <exalex16@gmail.com>
2025-02-18 20:39:15 -0800

    remove connection

--- Backend/services.ts
+++ Backend/services.ts
@@ -8,7 +8,7 @@ import AWS from "aws-sdk";
 export const clinet = new MongoClient(process.env.DB_URI ?? "mongodb://localhost:27017")
 
 // AWS S3 Connection
-export const s3 = new AWS.S3({
-    region: process.env.AWS_REGION
-});
+// export const s3 = new AWS.S3({
+//     region: process.env.AWS_REGION
+// });
 
