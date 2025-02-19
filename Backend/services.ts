bb10d654567a280c5ed17829c4c5ac0efc220251
Exalex16 <exalex16@gmail.com>
2025-02-18 19:37:48 -0800

    try with s3 user

--- Backend/services.ts
+++ Backend/services.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-// import AWS from "aws-sdk";
+import AWS from "aws-sdk";
 // import dotenv from "dotenv";
 
 // dotenv.config();
@@ -7,9 +7,7 @@ import { MongoClient } from "mongodb";
 // MongoDB Connection
 export const clinet = new MongoClient(process.env.DB_URI ?? "mongodb://localhost:27017")
 
-// // AWS S3 Connection
-// export const s3 = new AWS.S3({
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     region: process.env.AWS_REGION,
-// });
+// AWS S3 Connection
+export const s3 = new AWS.S3({
+    region: process.env.AWS_REGION
+});
