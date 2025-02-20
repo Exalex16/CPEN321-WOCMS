baf1a38623f57dd3c939e153444465843f28d8ff
Exalex16 <exalex16@gmail.com>
2025-02-19 17:44:26 -0800

    test remove something

--- Backend/routes/imageRoutes.ts
+++ Backend/routes/imageRoutes.ts
@@ -1,19 +1,19 @@
 import { body, param } from "express-validator"
-import { imageController, uploadMiddleware } from "../controllers/imageController";
+// import { imageController, uploadMiddleware } from "../controllers/imageController";
 
-const controller = new imageController()
+// const controller = new imageController()
 
 export const imageRoutes = [
-    {
-        method: "post",
-        route: "/upload",
-        action: [uploadMiddleware, controller.uploadImage],
-        validation: []
-    },
-    {
-        method: "delete",
-        route: "/:key",
-        action: controller.deleteImage,
-        validation: [param("key").isString()]
-    }
+    // {
+    //     method: "post",
+    //     route: "/upload",
+    //     action: [uploadMiddleware, controller.uploadImage],
+    //     validation: []
+    // },
+    // {
+    //     method: "delete",
+    //     route: "/:key",
+    //     action: controller.deleteImage,
+    //     validation: [param("key").isString()]
+    // }
 ]
