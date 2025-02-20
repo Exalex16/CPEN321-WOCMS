e826c54ffc78182b3b54a41c39e26208f086d822
Exalex16 <exalex16@gmail.com>
2025-02-19 21:43:11 -0800

    try add credential manually

--- Backend/services.ts
+++ Backend/services.ts
@@ -11,8 +11,11 @@ export const clinet = new MongoClient(process.env.DB_URI ?? "mongodb://localhost
 
 // AWS S3 Connection
 export const s3 = new S3Client({
-    region: 'us-west-2', // Set your AWS region
-    credentials: undefined, // Ensure it uses default credentials
+    region: "us-west-2", // Set your AWS region
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      }
 });
 
 const storage = multer.memoryStorage();
