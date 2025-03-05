fe4f1c75fb66dc56f383ec50f967ab611b7adfaf
Exalex16 <exalex16@gmail.com>
2025-03-05 14:46:43 -0800

    modify type

--- Backend/routes/userRoutes.ts
+++ Backend/routes/userRoutes.ts
@@ -58,7 +58,7 @@ export const userRoutes = [
         action: controller.removeLocation,
         validation: [
             param("googleEmail").isString(),
-            body("location").isString()
+            // body("location").isString()
         ],
     },
 ]
