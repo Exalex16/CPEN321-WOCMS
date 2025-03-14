654d33cb4815b8ab079128fb71678097ffc7a2e8
Exalex16 <exalex16@gmail.com>
2025-03-14 00:51:37 -0700

    try bound fix

--- Backend/controllers/mapController.ts
+++ Backend/controllers/mapController.ts
@@ -4,10 +4,14 @@ import { clinet } from "../services";
 
 
 export class mapController {
+    constructor() {
+        this.getRecommendation = this.getRecommendation.bind(this);
+    }
+
     /**
      * Get popular locations based on image density
      */
-    getRecommendation = async (req: Request, res: Response, next: NextFunction) => {
+    async getRecommendation(req: Request, res: Response, next: NextFunction) {
         const { userEmail } = req.params;
 
         const db = clinet.db("images");
